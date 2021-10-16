4b80ad0bfc7f8f0936e0bf59ee377f849a093f56
Malivix <mza2rintareh@gmail.com>
2021-10-16 10:27:55 +0330

    Delete dist directory

--- dist/dist/components/Example/Example.d.ts
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import './example.scss';
-export interface ExampleProps {
-    active?: boolean | undefined;
-    disabled?: boolean | undefined;
-    children?: any;
-    className?: string;
-}
-export declare const Example: React.FC<ExampleProps>;
